04e079ac39213b91291ec70bf1f20e941667952d
Brooke Warren <brookewarren@Brocs-MacBook-Pro.local>
2017-10-02 23:50:58 -0400

    done?

--- basicCard.js
+++ basicCard.js
@@ -1,6 +1,13 @@
 function BasicCard (front, back) {
+	if (this instanceof BasicCard) {
 	this.front = front;
 	this.back = back;
+} else {
+	return new BasicCard (front, back);
+}
 };
+
+var flashCard = new BasicCard("The apple is red", "red");
+var flashCard2 = BasicCard("The apple is red", "red");
 
 module.exports = BasicCard;
--- clozeCard.js
+++ clozeCard.js
@@ -1,23 +1,17 @@
 function ClozeCard (text, cloze) {
-	var partialText = function() {
-		var splitText = text.split(cloze);
-		if(splitText[0] === cloze) {
-			return " ... " + splitText[1];
-		}
-		else if(splitText[1] === cloze) {
-			return splitText[0] + " ... ";
-		}
-		else {
+	if (this instanceof ClozeCard){
+		if(!text.toLowerCase().includes(cloze.toLowerCase())) {
 			var errorMessage = cloze + " does not appear in " + text;
 			console.log(errorMessage);
-			alert(errorMessage);
 			throw new Error(errorMessage);
 		}
-	}
 	this.cloze = cloze;
-	this.partial = partialText;
+	this.partial = text.replace(cloze, " ... ");
 	this.fullText = text;
+} else {
+	return new ClozeCard (text, cloze);
 }
+};
 
 
 module.exports = ClozeCard;
